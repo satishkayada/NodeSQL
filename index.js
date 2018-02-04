0861125fd3a503c73af60014a6a005c00e2f5fe3
satish <satish.kayada@gmail.com>
2018-02-04 21:46:37 +0530

    First Commit With Acc UI

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
 
 //Body parser Middleware
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
